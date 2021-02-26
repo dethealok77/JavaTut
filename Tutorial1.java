e48515daab65fdb105d1c8a2a64fc388cbcc2cbb
alok <musykworld@gmail.com>
2021-02-27 01:35:34 +0530

    chnages

---
+++ Tutorial1.java
@@ -0,0 +1,18 @@
+package com.treemode.kpro.Controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author alokdethe
+ */
+public class Tutorial1 {
+   
+   public static void main(String[] args){
+   	System.out.println("This is Java Tutorial ");
+   }
+    
+}
+
