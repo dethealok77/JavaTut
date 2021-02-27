9eb6e751f3a301ff9eabe45f6508c8cdec457a17
alok <musykworld@gmail.com>
2021-02-27 16:55:59 +0530

    chnages

---
+++ TutEncapsulation.java
@@ -0,0 +1,22 @@
+package com.treemode.kpro.Controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author alokdethe
+ */
+ 
+ /*
+ 
+ */
+public class TutEncapsulation {
+   
+   public static void main(String[] args){
+   	System.out.println("This is Java Tutorial ");
+   }
+    
+}
+
